2a4325b61d73a88bb8be2bd2f7b6bbd234c784c8
suzel <64069582+suzelkhan1177@users.noreply.github.com>
2023-02-26 09:10:32 -0500

    let

--- assets/js/profile.js
+++ assets/js/profile.js
@@ -17,13 +17,13 @@ window.addEventListener('load', () => {
      .then((notes) => {
        console.log(notes);
        removeChildElements(notes_view);
-       for(var i=0; i<notes.length; i++) {
+       for(let i=0; i<notes.length; i++) {
               var new_div = document.createElement('div');
               var new_notes_id = document.createElement('p');
               new_notes_id.innerHTML = notes[i].name;
              new_div.appendChild(new_notes_id);
              new_div.style.border = '1px solid black';
-             var  filename =notes[i].file;    
+             let  filename =notes[i].file;    
              new_notes_id.addEventListener('click', (e) => {
                    console.log(e.target);
                  window.location  = `/users/notes/show_single_notes/${filename}`;
